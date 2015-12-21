f43bcac612eab8becf20dcdade3132bf3741e3d2
Neyanbhbin <yongjie.kanng@gmail.com>
2015-12-20 23:22:35 -0600

    add insertion sort shifts computation

---
+++ Algorithms/Sorting/quicksort_partition.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+################################################################################
+# Quicksort 1 - Partition
+# Ulr:https://www.hackerrank.com/challenges/quicksort1
+################################################################################
+
+
+def partition(arr):
+    pass
+
+m = int(raw_input())
+arr = [int(i) for i in raw_input().strip().split()]
+partition(arr)
